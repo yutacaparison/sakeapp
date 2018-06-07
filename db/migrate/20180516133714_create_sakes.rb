ffcf2491ea3b9976ea37c7a5f808a290ffd4ba3a
齋藤雄太 <yuta@yuta-saito.local>
2018-06-07 22:43:43 +0900

    外部キーにnull制約を入れる closed #3

--- db/migrate/20180516133714_create_sakes.rb
+++ db/migrate/20180516133714_create_sakes.rb
@@ -1,12 +1,12 @@
 class CreateSakes < ActiveRecord::Migration[5.1]
   def change
     create_table :sakes do |t|
-      t.string :sake
+      t.string :sake, null: false
       t.string :brewery
-      t.string :prefectures
+      t.string :prefectures, null: false
       t.text :image
-      t.text :comment
-      t.references :user, foreign_key: true
+      t.text :comment, null: false
+      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -41,12 +41,12 @@ ActiveRecord::Schema.define(version: 20180520103015) do
   end
 
   create_table "sakes", force: :cascade do |t|
-    t.string "sake"
+    t.string "sake", null: false
     t.string "brewery"
-    t.string "prefectures"
+    t.string "prefectures", null: false
     t.text "image"
-    t.text "comment"
-    t.bigint "user_id"
+    t.text "comment", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_sakes_on_user_id"
@@ -58,9 +58,9 @@ ActiveRecord::Schema.define(version: 20180520103015) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "password_digest"
+    t.string "name", null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
     t.text "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
